f72609df2ae7e913e866f501162c92070ec9c06e
CUWSulli <78753434+CUWSulli@users.noreply.github.com>
2021-02-21 16:48:39 -0600

    Initial commit

---
+++ src/Driver.java
@@ -0,0 +1,42 @@
+import java.net.URL;
+import org.json.simple.JSONValue;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Driver 
+{
+
+	public static void main(String[] args)
+	{ 
+		URLReader hearthstoneURLReader = new URLReader("https://api.hearthstonejson.com/v1/25770/enUS/cards.json");
+		//System.out.println(hearthstoneURLReader.getTheURLContents());
+		Object obj = JSONValue.parse(hearthstoneURLReader.getTheURLContents());
+		
+	    //System.out.println(obj instanceof JSONArray);
+	    if(obj instanceof JSONArray)
+	    {
+	    	//I am only in here if obj IS a JSONArray
+	    	JSONArray array = (JSONArray)obj;
+	    	int count = 0;
+	    	
+		    for(int i = 0; i < array.size(); i++)
+		    {
+		    	JSONObject cardData = (JSONObject)array.get(i);
+		    	if(cardData.containsKey("cost") && cardData.containsKey("name"))
+		    	{
+		    		if(cardData.containsKey("type") && cardData.get("type").equals("MINION"))
+		    		{
+		    			HearthstoneCard[] theMinions = new HearthstoneCard[3500];
+		    			Arrays.fill(theMinions, cardData.get("name"));
+
+		    		}
+		    	}
+		    	
+		    }
+		    System.out.println(count);
+	    }
+	}
+}
